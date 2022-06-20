f26b44907fd5cd006bcb9a7b3a263196db19ce8e
nxm67420 <46074688+nxm67420@users.noreply.github.com>
2022-06-19 22:56:52 -0500

    Start of Project

---
+++ src/test/java/com/mycompany/MywebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
